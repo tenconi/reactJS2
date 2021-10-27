f785329bbbbffc0795b387d1af89f7498ae5fc3e
tenconi <47565305+tenconi@users.noreply.github.com>
2021-10-27 19:26:30 -0300

    push con error

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/navBar/NavBar.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <p>asdasd</p>
+    </div>
+  );
+}
+
+export default App;
